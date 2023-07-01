23918f2c3ef33f1bc4a1241da937d2194b75df8d
roritopra <1193102598@u.icesi.edu.co>
2023-06-30 19:20:34 -0500

    added work

--- src/assets/main.js
+++ src/assets/main.js
@@ -24,7 +24,6 @@ async function fetchData(urlApi) {
 
 //Call to the Api and sampling on screen with the asynchronous method "then".
 
-/*
 
 function obtainCoins() {
     const apiUrl = ('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false');
@@ -41,15 +40,30 @@ function obtainCoins() {
 obtainCoins()
 
 function showCoin(coins) {
-    const resultDataElement = document.getElementById('result-data');
+    const resultDataElement = document.getElementsByClassName('Table-content-coins-all');
     let resultHTML = '';
 
     for (let i = 0; i < coins.length; i++) {
         resultHTML += `
-        <p>${coins[i].name}</p>
+        <div class="order">
+              <div class="Table-content-coins-left">
+                <p>1</p>
+                <img src="${coins[i].image}" alt="">
+                <p>${coins[i].symbol}</p>
+              </div>
+              <div class="Table-content-coins-right">
+                <p>${coins[i].current_price}</p>
+                <p>${coins[i].price_change_percentage_24h}</p>
+                <p>${coins[i].total_supply}</p>
+              </div>
+            </div>
+          <div class="border"></div>
         `;  
     }
 
     resultDataElement.innerHTML = resultHTML;
 }
-*/
+
+
+/*
+<p>${coins[i].name}*/
