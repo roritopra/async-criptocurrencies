ecbc8d8f82af461ba2c4213bc8979f75ffebcd86
roritopra <1193102598@u.icesi.edu.co>
2023-07-01 00:54:48 -0500

    added format number and function to aprox num

--- src/assets/main.js
+++ src/assets/main.js
@@ -40,41 +40,51 @@ function obtainCoins() {
 obtainCoins()
 
 function showCoin(coins) {
-    const resultDataElement = document.getElementById('Table-content-coins-all');
-  
-    let resultHTML = '';
-  
-    for (let i = 0; i < coins.length; i++) {
-      const currentPrice = coins[i].current_price;
-      const priceChange = coins[i].price_change_percentage_24h;
-      const totalSupply = coins[i].total_supply;
-      const symbol = coins[i].symbol.toUpperCase();
-  
-      // Verificar si los valores son positivos o negativos y aplicar los estilos correspondientes
-      const currentPriceColor = currentPrice < 0 ? 'red' : '#00EBA6';
-      const priceChangeColor = priceChange < 0 ? 'red' : '#00EBA6';
-      const totalSupplyColor = totalSupply < 0 ? 'red' : '#00EBA6';
-  
-      resultHTML += `
-        <div class="order">
-          <div class="Table-content-coins-left">
-            <p>1</p>
-            <img src="${coins[i].image}" alt="">
-            <p>${coins[i].name}</p>
-            <span>${symbol}</span>
-          </div>
-          <div class="Table-content-coins-right">
-            <p class="price" style="color: ${currentPriceColor}">${currentPrice}</p>
-            <p class="price_change" style="color: ${priceChangeColor}">${priceChange}</p>
-            <p class="supply" style="color: ${totalSupplyColor}">${totalSupply}</p>
-          </div>
+  const resultDataElement = document.getElementById('Table-content-coins-all');
+
+  let resultHTML = '';
+
+  for (let i = 0; i < coins.length; i++) {
+    const currentPrice = coins[i].current_price;
+    const priceChange = coins[i].price_change_percentage_24h;
+    const totalSupply = coins[i].total_supply;
+    const symbol = coins[i].symbol.toUpperCase();
+
+    let supply_number = ('000000000' + totalSupply).slice(-9);
+    
+    const currentPriceColor = currentPrice < 0 ? 'red' : '#00EBA6';
+    const priceChangeColor = priceChange < 0 ? 'red' : '#00EBA6';
+    const totalSupplyColor = totalSupply < 0 ? 'red' : '#00EBA6';
+
+    resultHTML += `
+      <div class="order">
+        <div class="Table-content-coins-left">
+          <p class="id">${formatNumber(i + 0)}</p>
+          <img src="${coins[i].image}" alt="">
+          <p>${coins[i].name}</p>
+          <span>${symbol}</span>
         </div>
-        <div class="border"></div>
-      `;  
-    }
-  
-    resultDataElement.innerHTML = resultHTML;
+        <div class="Table-content-coins-right">
+          <p class="price" style="color: ${currentPriceColor}">${currentPrice}</p>
+          <p class="price_change" style="color: ${priceChangeColor}">${priceChange}</p>
+          <p class="supply" style="color: ${totalSupplyColor}">${supply_number}</p>
+        </div>
+      </div>
+      <div class="border"></div>
+    `;
   }
+
+  resultDataElement.innerHTML = resultHTML;
+}
+
+function formatNumber(number) {
+  if (number < 3) {
+    return (number + 1).toString();
+  } else {
+    return (number + 1) ;
+  }
+}
+
 
 
 /*
