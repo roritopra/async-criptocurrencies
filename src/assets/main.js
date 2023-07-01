a7128a14206066d5e4185635a7092b47c7ed41f4
roritopra <1193102598@u.icesi.edu.co>
2023-06-30 21:46:31 -0500

    Aded uppercase and change color

--- src/assets/main.js
+++ src/assets/main.js
@@ -40,29 +40,41 @@ function obtainCoins() {
 obtainCoins()
 
 function showCoin(coins) {
-    const resultDataElement = document.getElementsByClassName('Table-content-coins-all');
+    const resultDataElement = document.getElementById('Table-content-coins-all');
+  
     let resultHTML = '';
-
+  
     for (let i = 0; i < coins.length; i++) {
-        resultHTML += `
+      const currentPrice = coins[i].current_price;
+      const priceChange = coins[i].price_change_percentage_24h;
+      const totalSupply = coins[i].total_supply;
+      const symbol = coins[i].symbol.toUpperCase();
+  
+      // Verificar si los valores son positivos o negativos y aplicar los estilos correspondientes
+      const currentPriceColor = currentPrice < 0 ? 'red' : '#00EBA6';
+      const priceChangeColor = priceChange < 0 ? 'red' : '#00EBA6';
+      const totalSupplyColor = totalSupply < 0 ? 'red' : '#00EBA6';
+  
+      resultHTML += `
         <div class="order">
-              <div class="Table-content-coins-left">
-                <p>1</p>
-                <img src="${coins[i].image}" alt="">
-                <p>${coins[i].symbol}</p>
-              </div>
-              <div class="Table-content-coins-right">
-                <p>${coins[i].current_price}</p>
-                <p>${coins[i].price_change_percentage_24h}</p>
-                <p>${coins[i].total_supply}</p>
-              </div>
-            </div>
-          <div class="border"></div>
-        `;  
+          <div class="Table-content-coins-left">
+            <p>1</p>
+            <img src="${coins[i].image}" alt="">
+            <p>${coins[i].name}</p>
+            <span>${symbol}</span>
+          </div>
+          <div class="Table-content-coins-right">
+            <p class="price" style="color: ${currentPriceColor}">${currentPrice}</p>
+            <p class="price_change" style="color: ${priceChangeColor}">${priceChange}</p>
+            <p class="supply" style="color: ${totalSupplyColor}">${totalSupply}</p>
+          </div>
+        </div>
+        <div class="border"></div>
+      `;  
     }
-
+  
     resultDataElement.innerHTML = resultHTML;
-}
+  }
 
 
 /*
