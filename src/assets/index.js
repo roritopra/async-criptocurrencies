25ef38901cc8348d4b9e3a795f863602b4902cf2
roritopra <1193102598@u.icesi.edu.co>
2023-07-04 01:05:35 -0500

    added scroll reveal

---
+++ src/assets/index.js
@@ -0,0 +1,49 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.navbar', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+
+    sr.reveal('.Hero', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+
+    sr.reveal('.card1', {
+        duration: 1500,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+
+    sr.reveal('.card2', {
+        duration: 2500,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+
+    sr.reveal('.card3', {
+        duration: 3500,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+
+    sr.reveal('.Mobile-container', {
+        duration: 4000,
+        origin: 'right',
+        distance: '-100px'
+    });
+
+    sr.reveal('.Auto-type-container', {
+        duration: 4000,
+        origin: 'left',
+        distance: '-100px'
+    });
+
+    sr.reveal('.Contact', {
+        duration: 4000,
+        origin: 'top',
+        distance: '-100px'
+    });
